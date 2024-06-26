13e5dfc6488d54d5bd6a68fc7535ba1cad77185c
bdulong <baptistedulong@gmail.com>
2024-06-26 12:26:49 +0200

    rajout raccourci depuis landing (temporaire)

--- src/components/Landing/landing.jsx
+++ src/components/Landing/landing.jsx
@@ -1,12 +1,16 @@
 import '../../App.css'
 import Header from '../Header/header.jsx'
 import './landing.css'
+import { Link } from 'react-router-dom';
 
 export default function Landing() {
   return (
     <main>
       <Header />
       <h1>Bienvenue !</h1>
+      <Link to="/ACGSnenIHF">SEPHORA</Link>
+      <Link to="/CLwKZGLtUy">ANTONIO</Link>
+      <Link to="/ZdecCTnmIA">CHANEL</Link>
     </main>
   )
 }
