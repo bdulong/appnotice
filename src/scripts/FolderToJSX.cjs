409f99773e6d2f0f6986a077060651eea1064219
bdulong <baptistedulong@gmail.com>
2024-06-27 10:14:07 +0200

    rangement fichier FolderToJSX

--- src/scripts/FolderToJSX.cjs
+++ src/scripts/FolderToJSX.cjs
@@ -2,27 +2,35 @@ const fs = require("fs");
 const path = require("path");
 
 const dossiersPath = path.join(__dirname, "..", "dossiers");
-const dossiersjsxPath = path.join(__dirname, "..", "components", "dossiers_jsx");
+const dossiersjsxPath = path.join(
+  __dirname,
+  "..",
+  "components",
+  "dossiers_jsx"
+);
 
 // Fonction pour formater le nom du dossier
 const formatterNomDossier = (nomDossier) => {
   // Retirer tous les chiffres du nom
-  let formateNom = nomDossier.replace(/\d+/g, '');
+  let formateNom = nomDossier.replace(/\d+/g, "");
   // Retirer les tirets
-  formateNom = formateNom.replace(/-/g, '');
+  formateNom = formateNom.replace(/-/g, "");
   // Remplacer les underscores par des espaces
-  formateNom = formateNom.replace(/_/g, ' ');
+  formateNom = formateNom.replace(/_/g, " ");
   return formateNom.trim(); // Retirer les espaces en début et fin de chaîne
 };
 
 // Fonction pour générer le contenu par défaut avec le nom du dossier et les sous-dossiers
 const genererContenu = (nomDossier, sousDossiers) => {
-    const sousDossiersHTML = sousDossiers
-      .map((sd) => `<button className='CTA-notice'>{t(\`sousDossiers.${sd}\`)}</button>`)
-      .join("\n")
-      .replace(/\n/g, "\n\t\t\t\t\t"); // Ajout des tabulations pour les boutons
+  const sousDossiersHTML = sousDossiers
+    .map(
+      (sd) =>
+        `<button className='CTA-notice'>{t(\`sousDossiers.${sd}\`)}</button>`
+    )
+    .join("\n")
+    .replace(/\n/g, "\n\t\t\t\t\t"); // Ajout des tabulations pour les boutons dans le code
 
-    return `import React from 'react'
+  return `import React from 'react'
 import { useTranslation } from 'react-i18next';
 import './dossiers_jsx-style.css';
 import '../../App.css';
@@ -52,61 +60,69 @@ export default Page;
 
 // Vérifier les fichiers JSX existants dans le dossier dossiers_jsx
 fs.readdir(dossiersjsxPath, (err, fichiersJSXExistants) => {
+  if (err) {
+    console.error('Erreur lors de la lecture du dossier "dossiers_jsx":', err);
+    return;
+  }
+
+  // Convertir les noms des fichiers existants en un ensemble pour une recherche rapide
+  const fichiersExistantSet = new Set(fichiersJSXExistants);
+
+  // Lire le contenu du dossier dossiers
+  fs.readdir(dossiersPath, (err, dossiers) => {
     if (err) {
-        console.error('Erreur lors de la lecture du dossier "dossiers_jsx":', err);
-        return;
+      console.error('Erreur lors de la lecture du dossier "dossiers":', err);
+      return;
     }
 
-    // Convertir les noms des fichiers existants en un ensemble pour une recherche rapide
-    const fichiersExistantSet = new Set(fichiersJSXExistants);
+    dossiers.forEach((dossier) => {
+      // Vérifier si le dossier commence par 2 chiffres
+      if (!/^\d{2}/.test(dossier)) {
+        console.log(
+          `Le dossier "${dossier}" ne correspond pas au format attendu.`
+        );
+        return;
+      }
 
-    // Lire le contenu du dossier dossiers
-    fs.readdir(dossiersPath, (err, dossiers) => {
-        if (err) {
-            console.error('Erreur lors de la lecture du dossier "dossiers":', err);
+      // Prendre les 8 premiers caractères du nom du dossier
+      const nomFichierJSX = dossier.substring(0, 8) + ".jsx";
+      const cheminFichierJSX = path.join(dossiersjsxPath, nomFichierJSX);
+
+      // Vérifier si le fichier existe déjà dans dossiers_jsx
+      if (fichiersExistantSet.has(nomFichierJSX)) {
+        console.log(`Le fichier ${nomFichierJSX} existe déjà.`);
+      } else {
+        const dossierPath = path.join(dossiersPath, dossier);
+
+        // Lire les sous-dossiers dans chaque dossier principal
+        fs.readdir(dossierPath, (err, sousDossiers) => {
+          if (err) {
+            console.error(
+              `Erreur lors de la lecture du dossier "${dossierPath}":`,
+              err
+            );
             return;
-        }
+          }
 
-        dossiers.forEach((dossier) => {
-            // Vérifier si le dossier commence par 2 chiffres
-            if (!/^\d{2}/.test(dossier)) {
-                console.log(`Le dossier "${dossier}" ne correspond pas au format attendu.`);
-                return;
-            }
+          // Formater le nom du dossier
+          const nomDossierFormate = formatterNomDossier(dossier);
 
-            // Prendre les 8 premiers caractères du nom du dossier
-            const nomFichierJSX = dossier.substring(0, 8) + ".jsx";
-            const cheminFichierJSX = path.join(dossiersjsxPath, nomFichierJSX);
+          // Générer le contenu avec le nom du dossier formaté et les sous-dossiers
+          const contenu = genererContenu(nomDossierFormate, sousDossiers);
 
-            // Vérifier si le fichier existe déjà dans dossiers_jsx
-            if (fichiersExistantSet.has(nomFichierJSX)) {
-                console.log(`Le fichier ${nomFichierJSX} existe déjà.`);
+          // Créer un fichier avec le contenu généré
+          fs.writeFile(cheminFichierJSX, contenu, (err) => {
+            if (err) {
+              console.error(
+                `Erreur lors de la création du fichier ${nomFichierJSX}:`,
+                err
+              );
             } else {
-                const dossierPath = path.join(dossiersPath, dossier);
-
-                // Lire les sous-dossiers dans chaque dossier principal
-                fs.readdir(dossierPath, (err, sousDossiers) => {
-                    if (err) {
-                        console.error(`Erreur lors de la lecture du dossier "${dossierPath}":`, err);
-                        return;
-                    }
-
-                    // Formater le nom du dossier
-                    const nomDossierFormate = formatterNomDossier(dossier);
-
-                    // Générer le contenu avec le nom du dossier formaté et les sous-dossiers
-                    const contenu = genererContenu(nomDossierFormate, sousDossiers);
-
-                    // Créer un fichier avec le contenu généré
-                    fs.writeFile(cheminFichierJSX, contenu, (err) => {
-                        if (err) {
-                            console.error(`Erreur lors de la création du fichier ${nomFichierJSX}:`, err);
-                        } else {
-                            console.log(`Fichier ${nomFichierJSX} créé avec succès.`);
-                        }
-                    });
-                });
+              console.log(`Fichier ${nomFichierJSX} créé avec succès.`);
             }
+          });
         });
+      }
     });
+  });
 });
